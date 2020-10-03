8ff6beec3adaf6fce26aafc23eee8f67e09c74d6
leah4912 <leah29726@gmail.com>
2020-10-03 18:04:05 -0400

    test passes

--- DogWalker.js
+++ DogWalker.js
@@ -1,7 +1,25 @@
 class DogWalker {
     numberOfWalksCalculator(dogSizes){
-        let numberOfWalks = -1;
+        // array of sizes in any order
+        // to group together, use a map = tally of dog sizes
+        // iterate through count and determine amount of hours
+        let numberOfWalks = 0;
+        let sizesMap = {};
 
+// iterate and collect the sizes
+        for(let size of dogSizes ){
+            if (!sizesMap[size]){
+                sizesMap[size] = 1;
+            } else {
+                sizesMap[size]++;
+            }
+        }
+// count the duplicates
+        for(let i in sizesMap){
+            numberOfWalks += Math.ceil(sizesMap[i]/2);
+        }
+
+        console.log(numberOfWalks);
         return numberOfWalks;
     }
 }
--- dw.test.js
+++ dw.test.js
@@ -3,14 +3,14 @@ const DogWalker = require('./DogWalker');
 
 test("Test uno", () =>{
     let dw = new DogWalker();
-    let expected = dw.numberOfWalksCalculator([1, 1, 3, 1, 2, 1, 3, 3, 3, 3]);
-    let actual = 6;
+    let actual = dw.numberOfWalksCalculator([1, 1, 3, 1, 2, 1, 3, 3, 3, 3]);
+    let expected = 6;
     expect(expected).toEqual(actual);
 });
 
 test("Test dos", () =>{
     let dw = new DogWalker();
-    let expected = dw.numberOfWalksCalculator([10, 20, 10, 10, 30, 50, 10, 20, 30]);
-    let actual = 5;
+    let actual = dw.numberOfWalksCalculator([10, 20, 10, 10, 30, 50, 10, 20, 30]);
+    let expected = 5;
     expect(expected).toEqual(actual);
 });
